e0dcf72a61aa680608c0cbcc1b19988dc3d96d61
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-05-20 12:13:38 -0700

    Add: word-break-ii - submission-1

---
+++ Data Structures & Algorithms/word-break-ii/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        res = []
+        def backtrack(idx, sentence):
+            if idx == len(s):
+                sentence = " ".join(sentence)
+                res.append(sentence)
+                return
+            for i in range(idx, len(s) + 1):
+                if s[idx: i] in wordDict:
+                    sentence.append(s[idx: i])
+                    backtrack(i, sentence)
+                    sentence.pop()
+        backtrack(0, [])
+        return res
+                    
