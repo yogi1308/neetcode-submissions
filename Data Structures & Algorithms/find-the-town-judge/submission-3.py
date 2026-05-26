84652f1d5aea73bc296cd14d867ac5151f167758
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-05-26 12:48:27 -0700

    Add: find-the-town-judge - submission-3

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        table = {i: set() for i in range(1, n + 1)}
+        for i in trust: 
+            table[i[0]].add(i[1])
+        candidates = -1
+        for i in table:
+            if table[i] == set(): 
+                candidates = i
+                break
+        for i in table:
+            if i == candidates: continue
+            if candidates not in table[i]:
+                return -1
+        return candidates
