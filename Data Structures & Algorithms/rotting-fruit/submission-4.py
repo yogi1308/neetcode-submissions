7048b74eaa859d1955cdd8f3f587011f8e3d84e1
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-06-01 10:28:58 -0700

    Add: rotting-fruit - submission-4

---
+++ Data Structures & Algorithms/rotting-fruit/submission-4.py
@@ -0,0 +1,46 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        minutes = 0 
+        fresh = set()
+        rotten = []
+        for y in range(len(grid)):
+            for x in range(len(grid[y])):
+                if grid[y][x] == 1: fresh.add((x, y))
+                if grid[y][x] == 2: rotten.append((x, y))
+
+        if not fresh: return 0
+
+        q = deque()
+        for cell in rotten:
+            q.append(cell)
+
+        def add(x, y):
+            adj = []
+            if x - 1 >= 0 and grid[y][x - 1] == 1:
+                adj.append((x - 1, y))
+                grid[y][x - 1] = 2
+                fresh.remove((x - 1, y))
+            if y - 1 >= 0 and grid[y - 1][x] == 1:
+                adj.append((x, y - 1))
+                grid[y - 1][x] = 2
+                fresh.remove((x, y - 1))
+            if x + 1 < len(grid[0]) and grid[y][x + 1] == 1:
+                adj.append((x + 1, y))
+                grid[y][x + 1] = 2
+                fresh.remove((x + 1, y))
+            if y + 1 < len(grid) and grid[y + 1][x] == 1:
+                adj.append((x, y + 1))
+                grid[y + 1][x] = 2
+                fresh.remove((x, y + 1))
+            return adj
+
+        while q:
+            for _ in range(len(q)):
+                x, y = q.popleft()
+                adj = add(x, y)
+                for a in adj:
+                    q.append(a)
+            minutes += 1
+
+        if not fresh: return minutes - 1
+        return -1
