9115ec6c5007e86b641edda1cb59eb7639c433ca
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-06-04 16:16:30 -0700

    Add: open-the-lock - submission-2

---
+++ Data Structures & Algorithms/open-the-lock/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if "0000" in deadends: return -1
+        q = deque()
+        q.append(("0000", 0))
+        visited = set(("0000", 0))
+        def algo(num, intstr, i):
+            if intstr == "-1": intstr = "9"
+            elif intstr == "10": intstr = "0"
+            numCopy = num[0][:]
+            numCopy = num[0][:i] + str(intstr) + num[0][i+1:]
+            if numCopy == target: return num[1] + 1
+            if numCopy not in visited and numCopy not in deadends: 
+                q.append((numCopy, num[1] + 1))
+                visited.add(numCopy)
+
+        def bfs(num):
+            int1, int2, int3, int4 = int(num[0][0]), int(num[0][1]), int(num[0][2]), int(num[0][3])
+            nums = [int1, int2, int3, int4]
+            for i in range(len(nums)):
+                for intstr in [str(nums[i] + 1), str(nums[i] - 1)]:
+                    result = algo(num, intstr, i)  
+                    if result is not None: return result
+
+        while q:
+            result = bfs(q.popleft())
+            if result is not None: return result
+
+        return -1
