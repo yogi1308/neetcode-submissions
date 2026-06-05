245d2d7abee0d8ff5c4d4998bb476108dfabf1d0
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-06-05 10:41:00 -0700

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        hmap = {}
+        dfsDone = set()
+        found = False
+        def dfs(prereqs, visited):
+            nonlocal found
+            if found: return False
+            for course in prereqs:
+                if course in visited:
+                    print(course, visited)
+                    found = True
+                else: 
+                    visited.add(course)
+                    if course in hmap: dfs(hmap[course], visited)
+                    dfsDone.add(course)
+                    visited.remove(course)
+            return True
+
+        for course, prereq in prerequisites:
+            if course not in hmap:
+                hmap[course] = []
+            hmap[course].append(prereq)
+
+        print(hmap)
+        for prereq in hmap:
+            if prereq not in dfsDone:
+                dfs(hmap[prereq], {prereq})
+                if found: return False
+
+        return False if found else True
