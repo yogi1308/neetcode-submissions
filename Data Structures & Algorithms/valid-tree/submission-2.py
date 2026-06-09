ba9917b85d8ed855bd70fdb62c179b1062aca1a4
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-06-09 10:03:42 -0700

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n -1: return False
+
+        hmap = defaultdict(list)
+        for node1, node2 in edges:
+            hmap[node1].append(node2)
+            hmap[node2].append(node1)
+        visited = set()
+        print(hmap)
+
+        def dfs(start):
+            if start in visited:
+                return
+            visited.add(start)
+            for node in hmap[start]:
+                dfs(node)
+            return
+            
+            
+        dfs(0)
+        print(visited)
+        return len(visited) == n
